428bf59284ecd3f46208ae5027e3c5f9473248f1
chayaPaperman <ch6752838@gmail.com>
2024-05-06 18:55:27 +0300

    add user router

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,3 +1,22 @@
+from fastapi import APIRouter
+import app.services.user_service as user_service
+from app.models.user_model import User
+from app.models.userDetails_model import UserDetails
 
-if __name__ == '__main__':
-    print("hjh")
+user_router = APIRouter()
+
+
+@user_router.post("/signup")
+async def signup(user: User):
+    return user_service.signup(user)
+
+
+@user_router.post("/login")
+async def login\
+                (details: UserDetails):
+    return user_service.login(details)
+
+
+@user_router.put("/update_details")
+async def update_details(user: User):
+    return user_service.update_details(user)
