d78fee6216397025d970b513abbb8808e9101ab1
chayapaperman <ch6752838@gmail.com>
2024-05-12 18:50:12 +0300

    test login function and update_details function

--- tests/test_user.py
+++ tests/test_user.py
@@ -13,29 +13,60 @@ def test_signup():
         "email": "fcsdc@dfh.com",
         "password": "12345678"
     }
-    response = requests.post(BASE_URL+"/user/signup", data=json.dumps(data))
-    status_response = response.status_code
-    assert status_response == 200
+    response = requests.post(BASE_URL + "/user/signup", data=json.dumps(data))
+    assert response.status_code == 200
     data['age'] = 0
-    response = requests.post(BASE_URL+"/user/signup", data=json.dumps(data))
-    status_response = response.status_code
-    assert status_response == 422
+    response = requests.post(BASE_URL + "/user/signup", data=json.dumps(data))
+    assert response.status_code == 422
     data['age'] = 20
     data['email'] = "43545654"
-    response = requests.post(BASE_URL+"/user/signup", data=json.dumps(data))
-    status_response = response.status_code
-    assert status_response == 422
+    response = requests.post(BASE_URL + "/user/signup", data=json.dumps(data))
+    assert response.status_code == 422
     data['email'] = "fcsdc@dfh.com"
     data['password'] = "123456789012"
-    response = requests.post(BASE_URL+"/user/signup", data=json.dumps(data))
-    status_response = response.status_code
-    assert status_response == 422
+    response = requests.post(BASE_URL + "/user/signup", data=json.dumps(data))
+    assert response.status_code == 422
     data['password'] = "12345678"
     data['name'] = "ch"
-    response = requests.post(BASE_URL+"/user/signup", data=json.dumps(data))
-    status_response = response.status_code
-    assert status_response == 422
+    response = requests.post(BASE_URL + "/user/signup", data=json.dumps(data))
+    assert response.status_code == 422
     data['name'] = "chaya"
-    response = requests.post(BASE_URL+"/user/signup", data=json.dumps(data))
-    status_response = response.status_code
-    assert status_response == 409
+    response = requests.post(BASE_URL + "/user/signup", data=json.dumps(data))
+    assert response.status_code == 409
+
+
+def test_login():
+    data = {
+        "name": "chaya",
+        "password": "12345678"
+    }
+    response = requests.post(BASE_URL + "/user/login", data=json.dumps(data))
+    assert response.json() == 0
+    data['password'] = "123456789012"
+    response = requests.post(BASE_URL + "/user/login", data=json.dumps(data))
+    assert response.status_code == 404
+    data['password'] = "12345678"
+    data['name'] = "ch"
+    response = requests.post(BASE_URL + "/user/login", data=json.dumps(data))
+    assert response.status_code == 404
+
+
+def test_update_details():
+    userid = 0
+    data = {
+        "id": 0,
+        "name": "chaya",
+        "age": 20,
+        "city": "modiinElit",
+        "email": "fcsdc@dfhfgfh.com",
+        "password": "12345678"
+    }
+    response = requests.put(BASE_URL + f"/user/update_details/{userid}", data=json.dumps(data))
+    assert response.status_code == 200
+    userid = 100
+    response = requests.put(BASE_URL + f"/user/update_details/{userid}", data=json.dumps(data))
+    assert response.status_code == 404
+    userid = 0
+    data['name'] = "ch"
+    response = requests.put(BASE_URL + f"/user/update_details/{userid}", data=json.dumps(data))
+    assert response.status_code == 422
