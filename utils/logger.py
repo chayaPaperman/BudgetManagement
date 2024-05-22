ac6a58c081a42438f42184d32a0d7c0e1bb7ae7a
chayapaperman <ch6752838@gmail.com>
2024-05-22 18:54:33 +0300

    add statistics test

--- utils/logger.py
+++ utils/logger.py
@@ -1,17 +1,22 @@
 import functools
 import logging
-from fastapi import  Request,Response
+from fastapi import Request, Response
 
 logging.basicConfig(filename='utils/server.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 
 def log_request(func):
+    """
+    A decorator that logs to a file for each server call
+    """
+
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
-        request: Request = kwargs['request']  # Assuming the request object is the second argument
+        request: Request = kwargs['request']
         method = request.method
         path = request.url.path
         logging.info(f"{request.client.host}: {method} {path} ")
         response: Response = await func(*args, **kwargs)
         return response
+
     return wrapper
