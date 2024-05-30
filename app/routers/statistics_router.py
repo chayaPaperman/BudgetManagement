c9c0f6b577d982eb477985440c255b3ad9ff4052
chayaPaperman <ch6752838@gmail.com>
2024-05-30 13:08:25 +0300

    add get_user_balance  function

--- app/routers/statistics_router.py
+++ app/routers/statistics_router.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from app.services import statistics_service
+from app.utils.logger import log_request
 
 statistics_router = APIRouter()
 
 
-@statistics_router.get("/users_balance")
-async def balance_statistics():
+@statistics_router.get("/balance")
+@log_request
+async def get_users_balance(request: Request):
     return await statistics_service.get_users_balance()
 
+
+@statistics_router.get("/balance/{user_id}")
+@log_request
+async def get_user_balance(user_id, request: Request):
+    return await statistics_service.get_user_balance(user_id)
--- app/services/statistics_service.py
+++ app/services/statistics_service.py
@@ -10,7 +10,6 @@ async def get_users_balance():
     :return:A picture of the graph
     """
     users = await user_service.get_all_users()
-    [u.pop('_id') for u in users]
     users_names = []
     users_balance = []
     for u in users:
@@ -18,6 +17,29 @@ async def get_users_balance():
         users_balance.append(await operation_service.get_balance(u['id']))
     plt.bar(users_names, users_balance, color='red')
     plt.title('Users Balance Bar Graph')
+    plt.xlabel('Users')
+    plt.ylabel('Balance')
+    img_bytes = io.BytesIO()
+    plt.savefig(img_bytes, format='png')
+    img_bytes.seek(0)
+    plt.close()
+    return StreamingResponse(io.BytesIO(img_bytes.read()), media_type="image/png")
+
+
+async def get_user_balance(user_id):
+    """
+    A function that returns a bar graph describing the user's budget according to months
+    :param user_id: the id of the user
+    :return:A picture of the graph
+    """
+
+    x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    y = []
+    for i in x:
+        y.append(await operation_service.get_user_balance_by_month(user_id, i))
+    plt.style.use('fast')
+    plt.plot(x, y)
+    plt.title('User Balance Graph')
     plt.xlabel('Users')
     plt.ylabel('Balance')
     img_bytes = io.BytesIO()
